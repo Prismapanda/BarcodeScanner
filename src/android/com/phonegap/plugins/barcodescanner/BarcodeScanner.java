820d76f2f320e0ce779dd4dd42f27375539bdcac
Prismapanda <e-kastner@web.de>
2013-08-11 10:36:56 +0200

    Removed deprecated APIs for compliance with Cordova 3.0

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -8,145 +8,153 @@
 
 package com.phonegap.plugins.barcodescanner;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
-
-import org.apache.cordova.api.Plugin;
-import org.apache.cordova.api.PluginResult;
 
 /**
  * This calls out to the ZXing barcode reader and returns the result.
  */
-public class BarcodeScanner extends Plugin {
-    private static final String SCAN = "scan";
-    private static final String ENCODE = "encode";
-    private static final String CANCELLED = "cancelled";
-    private static final String FORMAT = "format";
-    private static final String TEXT = "text";
-    private static final String DATA = "data";
-    private static final String TYPE = "type";
-    private static final String SCAN_INTENT = "com.phonegap.plugins.barcodescanner.SCAN";
-    private static final String ENCODE_DATA = "ENCODE_DATA";
-    private static final String ENCODE_TYPE = "ENCODE_TYPE";
-    private static final String ENCODE_INTENT = "com.phonegap.plugins.barcodescanner.ENCODE";
-    private static final String TEXT_TYPE = "TEXT_TYPE";
-    private static final String EMAIL_TYPE = "EMAIL_TYPE";
-    private static final String PHONE_TYPE = "PHONE_TYPE";
-    private static final String SMS_TYPE = "SMS_TYPE";
+public class BarcodeScanner extends CordovaPlugin {
+	private static final String SCAN = "scan";
+	private static final String ENCODE = "encode";
+	private static final String CANCELLED = "cancelled";
+	private static final String FORMAT = "format";
+	private static final String TEXT = "text";
+	private static final String DATA = "data";
+	private static final String TYPE = "type";
+	private static final String SCAN_INTENT = "com.phonegap.plugins.barcodescanner.SCAN";
+	private static final String ENCODE_DATA = "ENCODE_DATA";
+	private static final String ENCODE_TYPE = "ENCODE_TYPE";
+	private static final String ENCODE_INTENT = "com.phonegap.plugins.barcodescanner.ENCODE";
+	private static final String TEXT_TYPE = "TEXT_TYPE";
+	private static final String EMAIL_TYPE = "EMAIL_TYPE";
+	private static final String PHONE_TYPE = "PHONE_TYPE";
+	private static final String SMS_TYPE = "SMS_TYPE";
 
-    public static final int REQUEST_CODE = 0x0ba7c0de;
+	public static final int REQUEST_CODE = 0x0ba7c0de;
 
-    public String callback;
+	public String callback;
+	public CallbackContext callbackContext;
 
-    /**
-     * Constructor.
-     */
-    public BarcodeScanner() {
-    }
+	/**
+	 * Constructor.
+	 */
+	public BarcodeScanner() {
+	}
 
-    /**
-     * Executes the request and returns PluginResult.
-     *
-     * @param action        The action to execute.
-     * @param args          JSONArray of arguments for the plugin.
-     * @param callbackId    The callback id used when calling back into JavaScript.
-     * @return              A PluginResult object with a status and message.
-     */
-    public PluginResult execute(String action, JSONArray args, String callbackId) {
-        this.callback = callbackId;
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		LOG.d("booky", "executing BarcodeScanner.execute");
+		this.callbackContext = callbackContext;
+		if (action.equals(ENCODE)) {
+			JSONObject obj = args.optJSONObject(0);
+			if (obj != null) {
+				String type = obj.optString(TYPE);
+				String data = obj.optString(DATA);
 
-        if (action.equals(ENCODE)) {
-            JSONObject obj = args.optJSONObject(0);
-            if (obj != null) {
-                String type = obj.optString(TYPE);
-                String data = obj.optString(DATA);
+				// If the type is null then force the type to text
+				if (type == null) {
+					type = TEXT_TYPE;
+				}
 
-                // If the type is null then force the type to text
-                if (type == null) {
-                    type = TEXT_TYPE;
-                }
+				if (data == null) {
+					callbackContext.error("User did not specify data to encode");
+				}
 
-                if (data == null) {
-                    return new PluginResult(PluginResult.Status.ERROR, "User did not specify data to encode");
-                }
+				encode(type, data);
+			} else {
+				callbackContext.error("User did not specify data to encode");
+			}
+		} else if (action.equals(SCAN)) {
+			scan();
+			return true;
+		} else {
+			return false; // Returning false results in a "MethodNotFound"
+							// error.
+		}
+		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.NO_RESULT));
+		return true;
 
-                encode(type, data);
-            } else {
-                return new PluginResult(PluginResult.Status.ERROR, "User did not specify data to encode");
-            }
-        }
-        else if (action.equals(SCAN)) {
-            scan();
-        } else {
-            return new PluginResult(PluginResult.Status.INVALID_ACTION);
-        }
-        PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
-        r.setKeepCallback(true);
-        return r;
-    }
+	}
 
+	/**
+	 * Starts an intent to scan and decode a barcode.
+	 */
+	public void scan() {
+		LOG.d("booky", "scanning Barcode");
+		Intent intentScan = new Intent(SCAN_INTENT);
+		intentScan.addCategory(Intent.CATEGORY_DEFAULT);
 
-    /**
-     * Starts an intent to scan and decode a barcode.
-     */
-    public void scan() {
-        Intent intentScan = new Intent(SCAN_INTENT);
-        intentScan.addCategory(Intent.CATEGORY_DEFAULT);
+		this.cordova.startActivityForResult(this, intentScan, REQUEST_CODE);
+	}
 
-        this.cordova.startActivityForResult((Plugin) this, intentScan, REQUEST_CODE);
-    }
+	/**
+	 * Called when the barcode scanner intent completes
+	 * 
+	 * @param requestCode
+	 *            The request code originally supplied to
+	 *            startActivityForResult(), allowing you to identify who this
+	 *            result came from.
+	 * @param resultCode
+	 *            The integer result code returned by the child activity through
+	 *            its setResult().
+	 * @param intent
+	 *            An Intent, which can return result data to the caller (various
+	 *            data can be attached to Intent "extras").
+	 */
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		try {
+			if (requestCode == REQUEST_CODE) {
+				if (resultCode == Activity.RESULT_OK) {
+					JSONObject obj = new JSONObject();
+					try {
+						obj.put(TEXT, intent.getStringExtra("SCAN_RESULT"));
+						obj.put(FORMAT, intent.getStringExtra("SCAN_RESULT_FORMAT"));
+						obj.put(CANCELLED, false);
+					} catch (JSONException e) {
+						// Log.d(LOG_TAG, "This should never happen");
+					}
+					this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, obj));
+				} else if (resultCode == Activity.RESULT_CANCELED) {
+					JSONObject obj = new JSONObject();
+					try {
+						obj.put(TEXT, "");
+						obj.put(FORMAT, "");
+						obj.put(CANCELLED, true);
+					} catch (JSONException e) {
+						// Log.d(LOG_TAG, "This should never happen");
+					}
+					this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, obj));
+				} else {
+					this.callbackContext.error("");
+				}
+			}
+		} catch (Exception e) {
+			LOG.e("this", "that", e);
+			this.callbackContext.error(e.getMessage());
+		}
+	}
 
-    /**
-     * Called when the barcode scanner intent completes
-     *
-     * @param requestCode       The request code originally supplied to startActivityForResult(),
-     *                          allowing you to identify who this result came from.
-     * @param resultCode        The integer result code returned by the child activity through its setResult().
-     * @param intent            An Intent, which can return result data to the caller (various data can be attached to Intent "extras").
-     */
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                JSONObject obj = new JSONObject();
-                try {
-                    obj.put(TEXT, intent.getStringExtra("SCAN_RESULT"));
-                    obj.put(FORMAT, intent.getStringExtra("SCAN_RESULT_FORMAT"));
-                    obj.put(CANCELLED, false);
-                } catch(JSONException e) {
-                    //Log.d(LOG_TAG, "This should never happen");
-                }
-                this.success(new PluginResult(PluginResult.Status.OK, obj), this.callback);
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                JSONObject obj = new JSONObject();
-                try {
-                    obj.put(TEXT, "");
-                    obj.put(FORMAT, "");
-                    obj.put(CANCELLED, true);
-                } catch(JSONException e) {
-                    //Log.d(LOG_TAG, "This should never happen");
-                }
-                this.success(new PluginResult(PluginResult.Status.OK, obj), this.callback);
-            } else {
-                this.error(new PluginResult(PluginResult.Status.ERROR), this.callback);
-            }
-        }
-    }
+	/**
+	 * Initiates a barcode encode.
+	 * 
+	 * @param data
+	 *            The data to encode in the bar code
+	 * @param data2
+	 */
+	public void encode(String type, String data) {
+		Intent intentEncode = new Intent(ENCODE_INTENT);
+		intentEncode.putExtra(ENCODE_TYPE, type);
+		intentEncode.putExtra(ENCODE_DATA, data);
 
-    /**
-     * Initiates a barcode encode.
-     * @param data  The data to encode in the bar code
-     * @param data2
-     */
-    public void encode(String type, String data) {
-        Intent intentEncode = new Intent(ENCODE_INTENT);
-        intentEncode.putExtra(ENCODE_TYPE, type);
-        intentEncode.putExtra(ENCODE_DATA, data);
-
-        this.cordova.getActivity().startActivity(intentEncode);
-    }
+		this.cordova.getActivity().startActivity(intentEncode);
+	}
 }
